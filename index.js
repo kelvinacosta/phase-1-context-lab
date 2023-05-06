cf594a93b7d1008e8519908577e98885bb9abea8
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-05-06 09:43:58 -0400

    complete a context lab walkthrough

--- index.js
+++ index.js
@@ -9,6 +9,69 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(employeeArray){
+    return {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+
+ }
+ function createEmployeeRecords(employeeArray){
+     /*
+     let employeeRecords = []
+     for(const employee of employeeArray){
+         employeeRecords.push(createEmployeeRecord(employee))
+     }
+     return employeeRecords
+*/
+
+     return employeeArray.map(createEmployeeRecord)
+ }
+
+ function createTimeInEvent(dateStamp){
+    //this is an employee
+    let date = dateStamp.split(' ')[0]
+    let hour = dateStamp.split(' ')[1]
+
+    this.timeInEvents.push({
+        type:"TimeIn",
+        hour:parseInt(hour),
+        date:date
+    })
+    return this 
+
+ }
+
+ function createTimeOutEvent(dateStamp){
+    let date = dateStamp.split(' ')[0]
+    let hour = dateStamp.split(' ')[1]
+
+    this.timeOutEvents.push({
+        type:"TimeOut",
+        hour:parseInt(hour),
+        date:date
+    })
+
+    return this 
+ }
+ 
+ function hoursWorkedOnDate(date){
+
+    const timeIn = this.timeInEvents.find(record => record.date === date)
+    const timeOut = this.timeOutEvents.find(record => record.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+function wagesEarnedOnDate(date){
+   const numbers =  hoursWorkedOnDate.call(this,date)
+   return numbers * this.payPerHour
+}
+
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +84,15 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(collection,firstNameString){
+    return collection.find(record => record.firstName === firstNameString)
+    
+}
+
+function calculatePayroll(records){
+    return records.reduce((accumulator,record)=>{
+
+        return allWagesFor.call(record) + accumulator
+    },0)
+
+}
